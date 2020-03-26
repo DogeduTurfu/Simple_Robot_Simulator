21b325e5360a28557def5e5589aaa594b10d226d
DogeduTurfu <romain.quachlux@gmail.com>
2020-03-26 11:58:19 +0100

    tests update

--- test.py
+++ test.py
@@ -3,3 +3,25 @@
 
 import model
 
+m = model.Model()
+
+m.m1.speed = 10
+m.m2.speed = 10
+
+print("Default model: {}".format(m))
+
+print("### Setting a speed to motor 1 only")
+m.m1.speed = 0.1
+linear_speed, rotational_speed = m.dk()
+print("linear_speed = {}\notational_speed = {}".format(linear_speed, rotational_speed))
+
+print("### Setting a speed to motor 2 only")
+m.m2.speed = 0.1
+linear_speed, rotational_speed = m.dk()
+print("linear_speed = {}\notational_speed = {}".format(linear_speed, rotational_speed))
+
+print("### Setting a speed to both motors")
+m.m1.speed = 0.1
+m.m2.speed = 0.1
+linear_speed, rotational_speed = m.dk()
+print("linear_speed = {}\notational_speed = {}".format(linear_speed, rotational_speed))
